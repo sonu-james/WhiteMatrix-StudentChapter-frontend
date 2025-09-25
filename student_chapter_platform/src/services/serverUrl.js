d2aab21e709fc683820ca26738f0ad0e6d6a4644
sonu-james <sonupjames@gmail.com>
2025-09-25 18:27:25 +0530

    serverUrl updated

--- student_chapter_platform/src/services/serverUrl.js
+++ student_chapter_platform/src/services/serverUrl.js
@@ -1,2 +1,3 @@
 // export const serverUrl='http://localhost:4000'
-export const serverUrl='https://whitematrix-studentchapter-backend-1.onrender.com'
+// export const serverUrl='https://whitematrix-studentchapter-backend-1.onrender.com'
+ export const serverUrl=' https://whitematrix-studentchapter-backend-1ois.onrender.com'
